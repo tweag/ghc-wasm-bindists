9da2ef3efe657b78ac3d650eb274b92f552b9d5a
Cheng Shao <terrorjack@type.dance>
2024-10-20 01:09:53 +0000

    Fetch ghc head from finished nightly pipelines

--- app/Main.hs
+++ app/Main.hs
@@ -258,7 +258,7 @@ bindistInfos =
                   ref = "master",
                   jobName = "nightly-x86_64-linux-alpine3_20-wasm-cross_wasm32-wasi-release+host_fully_static+text_simdutf",
                   artifactPath = "ghc-x86_64-linux-alpine3_20-wasm-cross_wasm32-wasi-release+host_fully_static+text_simdutf.tar.xz",
-                  pipelineFilter = [("source", Just "schedule")]
+                  pipelineFilter = [("scope", Just "finished"), ("source", Just "schedule")]
                 }
           },
       (,)
@@ -272,7 +272,7 @@ bindistInfos =
                   ref = "master",
                   jobName = "nightly-x86_64-linux-alpine3_20-wasm-int_native-cross_wasm32-wasi-release+host_fully_static+text_simdutf",
                   artifactPath = "ghc-x86_64-linux-alpine3_20-wasm-int_native-cross_wasm32-wasi-release+host_fully_static+text_simdutf.tar.xz",
-                  pipelineFilter = [("source", Just "schedule")]
+                  pipelineFilter = [("scope", Just "finished"), ("source", Just "schedule")]
                 }
           },
       (,)
@@ -286,7 +286,7 @@ bindistInfos =
                   ref = "master",
                   jobName = "nightly-x86_64-linux-alpine3_20-wasm-unreg-cross_wasm32-wasi-release+host_fully_static+text_simdutf",
                   artifactPath = "ghc-x86_64-linux-alpine3_20-wasm-unreg-cross_wasm32-wasi-release+host_fully_static+text_simdutf.tar.xz",
-                  pipelineFilter = [("source", Just "schedule")]
+                  pipelineFilter = [("scope", Just "finished"), ("source", Just "schedule")]
                 }
           },
       (,)
